cc7d84b73f3fca264a5a08ffeb99dc454288250d
atharvaBaste <msm19b024@iiitdm.ac.in>
2022-05-23 20:52:31 +0530

    Added Add button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
-
+import {Api} from './app.model'
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  templateUrl: './app.view.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'api-form';
+  Api : Api=new Api();
+
+  ApiDataStore: Array<Api>= new Array<Api>();
+  Add(){
+    this.ApiDataStore.push(this.Api);  /**first add the Api data */
+    this.Api= new Api(); /**Then clear old i/ps */
+
+  }
 }
---
+++ src/app/app.model.ts
@@ -0,0 +1,5 @@
+
+export class Api{
+    Apiname:string ="";
+    Apiurl:string="";
+}
